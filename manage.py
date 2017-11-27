e63a465ee44bdf137674d4524243799df1e945c2
Emily Zhang <jingwenz@udel.edu>
2017-11-27 16:19:41 -0500

    flask II added

--- manage.py
+++ manage.py
@@ -1,7 +1,25 @@
 from flask_script import Manager
-from resume import app
+from resume import app, db, Professor, Course
 
 manager = Manager(app)
+
+@manager.command
+def deploy():
+    db.drop_all()
+    db.create_all()
+    professor1 = Professor(name='Wang,Jiannan', department='MISY')
+    professor2 = Professor(name='Sharma,Pratyush Nidhi', department='MISY')
+    course1 = Course(course_number="MISY330",title='Database Design and Implementation', description='Covers the design and implementation of enterprise databases in the business environment. A networked setting and its effect on database management will be emphasized', professor=professor2)
+    course2 = Course(course_number="MISY350",title='Business Application Development II', description='Covers concepts related to client side development, including cascading style sheets and JavaScript.', professor=professor1)
+    course3 = Course(course_number="ECON301",title='Quantitative Microeconomic Theory', description='Uses calculus to study price determination and income distribution in a market economy; and the behavior of firms and industry under conditions of pure and imperfect competition.')
+    course4 = Course(course_number="MISY225",title='Introduction to Programming Business Applications', description='Use of higher level contemporary computing languages to structure Prototyping applications of systems.')
+    db.session.add(professor1)
+    db.session.add(professor2)
+    db.session.add(course1)
+    db.session.add(course2)
+    db.session.add(course3)
+    db.session.add(course4)
+    db.session.commit()
 
 if __name__ == "__main__":
     manager.run()
